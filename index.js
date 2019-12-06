d6b01544bb936c866b64728dfa49278de1d28679
Evan Halley <evan.halley@gmail.com>
2019-12-05 23:13:40 -0500

    cleaned up tensorflow integration

--- index.js
+++ index.js
@@ -2,76 +2,51 @@
 
 const HCCrawler = require('headless-chrome-crawler');
 const request = require('request');
-const fs = require('fs');
 const tf = require('@tensorflow/tfjs');
-require('@tensorflow/tfjs-node');
+const tfnode = require('@tensorflow/tfjs-node');
 const mobilenet = require('@tensorflow-models/mobilenet');
-const jpeg = require('jpeg-js');
+const extractDomain = require('extract-domain');
 
-const NUMBER_OF_CHANNELS = 3
+function getImagePixelData(imageUrl) {
+    return new Promise((resolve, reject) => {
 
+        let options = {
+            url: imageUrl,
+            method: "get",
+            encoding: null
+        };
 
-let model = null;
-
-function saveImage(url) {
-    let filename = `./tmp/${new Date().getTime()}.jpg`;
-    let file = fs.createWriteStream(filename);
-    request(url).pipe(file).on('close', async () => {
-
-        let image = readImage(filename);
-        const input = imageToInput(image, NUMBER_OF_CHANNELS);
-        let predictions = await model.classify(input);
-        console.log(`${filename} -> ${JSON.stringify(predictions)}`);
+        request(options, (err, response, buffer) => {
+        
+            if (err) {
+                reject(err)
+            } else {
+                resolve(buffer);
+            }
+        });
     });
 }
 
-const readImage = path => {
-    const buf = fs.readFileSync(path)
-    const pixels = jpeg.decode(buf, true)
-    return pixels
-  }
+async function crawlForImages(url) {
+    let domain = extractDomain(url);
+    console.log(domain);
+    let imageUrls = [];
+    let crawler = await HCCrawler.launch({
 
-const imageByteArray = (image, numChannels) => {
-    const pixels = image.data
-    const numPixels = image.width * image.height;
-    const values = new Int32Array(numPixels * numChannels);
-  
-    for (let i = 0; i < numPixels; i++) {
-      for (let channel = 0; channel < numChannels; ++channel) {
-        values[i * numChannels + channel] = pixels[i * 4 + channel];
-      }
-    }
-  
-    return values
-  }
-  
-  const imageToInput = (image, numChannels) => {
-    const values = imageByteArray(image, numChannels)
-    const outShape = [image.height, image.width, numChannels];
-    const input = tf.tensor3d(values, outShape, 'int32');
-  
-    return input
-  }
-  
-
-(async () => {
-    model = await mobilenet.load();
-    const crawler = await HCCrawler.launch({
         customCrawl: async (page, crawl) => {
-            // You can access the page object before requests
             await page.setRequestInterception(true);
+
             page.on('request', request => {
+
                 if (request.resourceType() == 'image') {
-                    saveImage(request.url());
-                    console.log(`Image url: ${request.url()}`);
-                    // TODO machine learning things here
+                    imageUrls.push(request.url());
                     request.abort();
                 } else {
                     request.continue();
                 }
             });
           // The result contains options, links, cookies and etc.
-          const result = await crawl();
+          let result = await crawl();
           // You can access the page object after requests
           result.content = await page.content();
           // You need to extend and return the crawled result
@@ -81,12 +56,44 @@ const imageByteArray = (image, numChannels) => {
           console.log(`Got ${result.options.url}.`);
         },
     });
-    // Queue a request
-    await crawler.queue({ 
-        url: 'https://www.apsofdurham.org/', 
+    let options = {
+        url: url, 
         maxDepth: 1, 
-        allowedDomains: [ 'www.apsofdurham.org' ],
-        followSitemapXml: true });
-    await crawler.onIdle(); // Resolved when no queue is left
-    await crawler.close(); // Close the crawler
-  })();
+        allowedDomains: [ domain, `www.${domain}` ]};
+    await crawler.queue(options);
+    console.log(`Starting crawl of ${url}...`);
+    await crawler.onIdle();
+    await crawler.close();
+    console.log(`Found ${imageUrls.length} images...`);
+    return imageUrls;
+}
+
+async function classifyImage(model, url) {
+    let prediction = [];
+
+    try {
+        let imageBuffer = await getImagePixelData(url);
+
+        if (imageBuffer) {
+            let input = tfnode.node.decodeImage(imageBuffer);
+            prediction = await model.classify(input);
+        }
+    } catch (err) {
+        //console.error(err);
+    }
+    return prediction;
+}
+
+(async () => {
+    let imageUrls = await crawlForImages('https://www.apsofdurham.org/');
+    let model = await mobilenet.load();
+
+    for (let url of imageUrls) {
+        console.log(url);
+        let prediction = await classifyImage(model, url);
+
+        if (prediction) {
+            console.log(`${url} -> ${JSON.stringify(prediction)}`);
+        }
+    }
+})();
